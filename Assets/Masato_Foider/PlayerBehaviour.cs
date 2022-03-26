97da8683548f92322f07e4c6516d14e1d4a23bbb
masato-1122 <70794504+masato-1122@users.noreply.github.com>
2022-03-26 13:41:19 +0900

    プレイヤースクリプト変更

--- Assets/Masato_Foider/PlayerBehaviour.cs
+++ Assets/Masato_Foider/PlayerBehaviour.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    //private Rigidbody2D rigidBody;
+    public float speed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       // rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else if( Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
     }
 }
