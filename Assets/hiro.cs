a6ea340d8fd898a16da06ba29dd4dcc65dacc46f
soma296 <soma.kawano.0904@gmail.com>
2022-03-26 15:17:50 +0900

    s

--- Assets/hiro.cs
+++ Assets/hiro.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hiro : MonoBehaviour
+class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 何かとぶつかった時に呼ばれるビルトインメソッド
+    void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // ゲームオブジェクトを削除するメソッド
+        Destroy(gameObject);
     }
 }
