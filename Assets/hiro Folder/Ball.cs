18780c5e0cd882a8b5069448131c89262a751e89
hiro000227 <hiro227001@gmail.com>
2022-03-26 16:44:36 +0900

    Revert "ball 増殖"
    This reverts commit a2757b10314aa70c760a105fea3177eb1d14256b.

--- Assets/hiro Folder/Ball.cs
+++ Assets/hiro Folder/Ball.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+class Ball : MonoBehaviour
 {
   public float speed = 5f;
   Rigidbody myRigidbody;
