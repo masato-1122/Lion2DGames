8cabb2e62fb3f33348a8990b180e1ccbc6b10528
soma296 <soma.kawano.0904@gmail.com>
2022-03-26 14:15:21 +0900

    ball

---
+++ Assets/Ball.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Ball : MonoBehaviour
+{
+  public float speed = 5f;
+  Rigidbody myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+      myRigidbody = GetComponent<Rigidbody>();
+      myRigidbody.velocity = new Vector3(speed, speed, 0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+  {
+
+  }
+}
